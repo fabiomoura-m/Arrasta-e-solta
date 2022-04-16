b1a99e08bece851e1441ce725aee23c636c49a6f
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-04-16 15:11:36 -0300

    feat:add função que verifica se a ordem dos itens está correta

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,12 @@
 //   console.log(e.currentTarget); // retorna quem tem o evento, mesmo que tenha clicado em um item que está contido na área
 // });
 
+let areas = {
+  a: null,
+  b: null,
+  c: null
+};
+
 document.querySelectorAll('.item').forEach(item => {
   item.addEventListener('dragstart', dragStart);
   item.addEventListener('dragend', dragEnd);
@@ -47,6 +53,7 @@ function drop(e) {
   if (e.currentTarget.querySelector('.item') === null) {
     let dragItem = document.querySelector('.item.dragging');
     e.currentTarget.appendChild(dragItem);
+    updateAreas();
   }
 }
 
@@ -62,4 +69,22 @@ function dropNeutral(e) {
   e.currentTarget.classList.remove('hover');
   let dragItem = document.querySelector('.item.dragging');
   e.currentTarget.appendChild(dragItem);
+  updateAreas();
+}
+
+// Logic Functions
+function updateAreas() {
+  document.querySelectorAll('.area').forEach(area => {
+    let name = area.getAttribute('data-name');
+
+    if (area.querySelector('.item') !== null) {
+      areas[name] = area.querySelector('.item').innerHTML;
+    } else {
+      areas[name] = null;
+    }
+  });
+
+  if (areas.a === '1' && areas.b === '2' && areas.c === '3') {
+    document.querySelector('.areas').classList.add('correct');
+  }
 }
